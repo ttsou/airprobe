52afa1c28c086eb1051fdc801a69384396f5076a
Piotr Krysik <perper@o2.pl>
2009-06-06 23:07:09 +0200

    some random changes to the bulidsystem - resolve libtool imcompatibilities between different versions

---
+++ src/lib/gsm_receiver_config.h
@@ -0,0 +1,11 @@
+//
+// C++ Implementation: gsm_receiver_config
+//
+// Description: 
+//
+//
+// Author: Piotr Krysik <perper@o2.pl>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
