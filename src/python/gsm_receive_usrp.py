c473876ed253c3c70143991e35f9faa9c34c6290
Piotr Krysik <perper@o2.pl>
2009-06-13 19:30:40 +0200

    changed polish names of functions to english

--- src/python/gsm_receive_usrp.py
+++ src/python/gsm_receive_usrp.py
@@ -2,6 +2,7 @@
 
 from gnuradio import gr, gru, blks2
 #, gsm
+from gnuradio import usrp
 from gnuradio.eng_option import eng_option
 from optparse import OptionParser
 from os import sys
@@ -10,6 +11,13 @@ for extdir in ['../../debug/src/lib','../../debug/src/lib/.libs']:
     if extdir not in sys.path:
         sys.path.append(extdir)
 import gsm                        
+
+def pick_subdevice(u):
+    if u.db[0][0].dbid() >= 0:
+        return (0, 0)
+    if u.db[1][0].dbid() >= 0:
+        return (1, 0)
+    return (0, 0)
 
 class tune(gr.feval_dd):
     def __init__(self, top_block):
@@ -44,9 +52,28 @@ class gsm_receiver_first_blood(gr.top_block):
         return ujscie
     
     def _set_source(self):
-        nazwa_pliku = self.options.inputfile
-        zrodlo = gr.file_source(gr.sizeof_gr_complex, nazwa_pliku, False)
-        return zrodlo
+        options = self.options
+        fusb_block_size = gr.prefs().get_long('fusb', 'block_size', 4096)
+        fusb_nblocks    = gr.prefs().get_long('fusb', 'nblocks', 16)
+        self.usrp = usrp.source_c(decim_rate=options.decim, fusb_block_size=fusb_block_size, fusb_nblocks=fusb_nblocks)
+        
+        if options.rx_subdev_spec is None:
+            options.rx_subdev_spec = pick_subdevice(self.usrp)
+        
+        self.usrp.set_mux(usrp.determine_rx_mux_value(self.usrp, options.rx_subdev_spec))
+        # determine the daughterboard subdevice
+        self.subdev = usrp.selected_subdev(self.usrp, options.rx_subdev_spec)
+        input_rate = self.usrp.adc_freq() / self.usrp.decim_rate()
+
+        # set initial values
+        if options.gain is None:
+            # if no gain was specified, use the mid-point in dB
+            g = self.subdev.gain_range()
+            options.gain = float(g[0]+g[1])/2
+
+        r = self.usrp.tune(0, self.subdev, options.freq)
+        self.subdev.set_gain(options.gain)
+        return self.usrp
     
     def _set_rates(self):
         options = self.options
@@ -80,13 +107,19 @@ class gsm_receiver_first_blood(gr.top_block):
     def _process_options(self):
         parser = OptionParser(option_class=eng_option)
         parser.add_option("-d", "--decim", type="int", default=128,
-                          help="Set USRP decimation rate to DECIM [default=%default]")
+                                    help="Set USRP decimation rate to DECIM [default=%default]")
+        parser.add_option("-I", "--inputfile", type="string", default="cfile",
+                                    help="Input filename")
+        parser.add_option("-O", "--outputfile", type="string", default="cfile2.out",
+                                    help="Output filename")
+        parser.add_option("-R", "--rx-subdev-spec", type="subdev", default=None,
+                                    help="Select USRP Rx side A or B (default=first one with a daughterboard)")   
         parser.add_option("-r", "--osr", type="int", default=4,
                           help="Oversampling ratio [default=%default]")
-        parser.add_option("-I", "--inputfile", type="string", default="cfile",
-                          help="Input filename")
-        parser.add_option("-O", "--outputfile", type="string", default="cfile2.out",
-                          help="Output filename")
+        parser.add_option("-f", "--freq", type="eng_float", default="950.4M",
+                                    help="set frequency to FREQ", metavar="FREQ")
+        parser.add_option("-g", "--gain", type="eng_float", default=None,
+                                    help="Set gain in dB (default is midpoint)")
         (options, args) = parser.parse_args ()
         return (options, args)
     
